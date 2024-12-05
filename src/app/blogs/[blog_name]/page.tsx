a70785a2318ef6d7200cad2534ac68c067d2b962
Muhammad Muzzamil Bukhari <muhammadmuzzmilbukhari@gmail.com>
2024-12-05 11:28:24 +0500

    g

--- src/app/blogs/[blog_name]/page.tsx
+++ src/app/blogs/[blog_name]/page.tsx
@@ -4,7 +4,8 @@ const BlogPage = async ({ params }: { params: { blog_name: string } }) => {
   const { blog_name } = params;
   return (
     <>
-      <BlogPageComp slug={blog_name} />;
+      <h1>{blog_name}</h1>
+      {/* <BlogPageComp slug={blog_name} />; */}
     </>
   );
 };
